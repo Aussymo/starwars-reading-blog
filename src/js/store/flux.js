f8c198666b473f70e77df335e25341dd3a72c4a4
Aussymo <aussymo@gmail.com>
2022-07-25 18:05:46 +0000

    my first commit

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,6 +1,7 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
+
 			demo: [
 				{
 					title: "FIRST",
@@ -12,34 +13,88 @@ const getState = ({ getStore, getActions, setStore }) => {
 					background: "white",
 					initial: "white"
 				}
-			]
+			],
+			characters: [],
+			character: [],
+			planets: [],
+			planet: [],
+			vehicles: [],
+			vehicle: [],
+			favorites: [],
 		},
 		actions: {
 			// Use getActions to call a function within a fuction
 			exampleFunction: () => {
 				getActions().changeColor(0, "green");
 			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
+			loadCharacters: () => {
+				fetch("https://www.swapi.tech/api/people")
+					.then(res => res.json())
+					.then(data => {
+						console.log(data)
+						setStore({ characters: data.results })
+					})
+					.catch(err => console.error(err))
 			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
+			loadCharacter: (id) => {
+				fetch(`https://www.swapi.tech/api/people/${id}`)
+					.then(res => res.json())
+					.then(data => {
+						console.log(data)
+						setStore({ character: data.result.properties })
+					})
+					.catch(err => console.error(err))
+			},
+			loadPlanets: () => {
+				fetch("https://www.swapi.tech/api/planets")
+					.then(res => res.json())
+					.then(data => {
+						console.log(data)
+						setStore({ planets: data.results })
+					})
+					.catch(err => console.error(err))
+			},
+			loadPlanet: (id) => {
+				fetch(`https://www.swapi.tech/api/planets/${id}`)
+					.then(res => res.json())
+					.then(data => {
+						console.log(data)
+						setStore({ planet: data.result.properties })
+					})
+					.catch(err => console.error(err))
+			},
+			loadVehicles: () => {
+				fetch("https://www.swapi.tech/api/vehicles")
+					.then(res => res.json())
+					.then(data => {
+						console.log(data)
+						setStore({ vehicles: data.results })
+					})
+					.catch(err => console.error(err))
+			},
+			loadVehicle: (id) => {
+				fetch(`https://www.swapi.tech/api/vehicles/${id}`)
+					.then(res => res.json())
+					.then(data => {
+						console.log(data)
+						setStore({ vehicle: data.result.properties })
+					})
+					.catch(err => console.error(err))
+			},
+			favoritesInfo: item => {
+				console.log(item)
+				let myFavorites = getStore().favorites;
+				let selected = myFavorites.find(element => element === item);
+					if (selected) {
+						myFavorites = myFavorites.filter(element => item !== element);
+						setStore({ favorites: myFavorites });
+					} else {
+						myFavorites = [...myFavorites, item];
+						setStore({ favorites: myFavorites });
+					}},
+		},
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
-			}
-		}
 	};
 };
 
-export default getState;
+export default getState
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,24 @@
 import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "../../styles/home.css";
+import {Characters} from "./characters.js"
+import {Planets} from "./planets.js"
+import {Vehicles} from "./vehicles.js"
 
-export const Home = () => (
+export const Home = () => {
+	return (
 	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+		<h1>Character</h1>
+		<div className = "scroll">
+			<Characters />
+		</div>
+		<h1>Planets</h1>
+		<div className = "scroll">
+			<Planets />
+		</div>
+		<h1>Vehicles</h1>
+		<div className = "scroll">
+			<Vehicles />
+		</div>
+	</div> )
+};
